bb49fe2346e67f55153cd8fdbccad00f677518a0
User <user@vagelos-ve735-0635.apnd.wlan.private.upenn.edu>
2018-06-05 16:10:18 -0400

    added more loops to loop.py

--- python/loops.py
+++ python/loops.py
@@ -1,7 +1,18 @@
 nums=[1,2,3,4,5]
 results=[]
+data = []
+hilt_class = ['Isis', 'Natalia']
 
 for items in nums:
-    results.append(item+5)
+    results.append(items+5)
 
 print(results)
+
+for items in nums:
+    data.append(items-10)
+
+print(data)
+
+for name in hilt_class:
+    if name == 'Natalia':
+        print(name)
