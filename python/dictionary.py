9f4d2095394451e9ecbae4fb608ed92b01ef405c
User <user@vagelos-ve735-0635.apnd.wlan.private.upenn.edu>
2018-06-05 15:28:15 -0400

    loops file

--- python/dictionary.py
+++ python/dictionary.py
@@ -1,1 +1,6 @@
 my info = {'first_name':'Lola', 'last_name':'Lopez', 'hometown':'Macondo','favorite_food':'peanut butter'}
+
+instructors=['brandon','ethan']
+instructors.append('tony')
+
+my_info['favorite_instructor']='tony'
---
+++ python/loops.py
@@ -0,0 +1,7 @@
+nums=[1,2,3,4,5]
+results=[]
+
+for items in nums:
+    results.append(item+5)
+
+print(results)
