77c8ef8466d8ccfde1c6350a45a2e5174bbf1df6
User <user@vagelos-ve735-0635.apnd.wlan.private.upenn.edu>
2018-06-06 10:17:35 -0400

    added new functions

--- python/loops.py
+++ python/loops.py
@@ -1,18 +1,39 @@
-nums=[1,2,3,4,5]
-results=[]
-data = []
-hilt_class = ['Isis', 'Natalia']
+# nums=[1,2,3,4,5]
+# results=[]
+# data = []
+# hilt_class = ['Isis', 'Natalia']
+#
+# for items in nums:
+#     results.append(items+5)
+#
+# print(results)
+#
+# for items in nums:
+#     data.append(items-10)
+#
+# print(data)
+#
+# for name in hilt_class:
+#     if name == 'Natalia':
+#         print(name)
 
-for items in nums:
-    results.append(items+5)
+days={"Monday":"dance", "Tuesday":"kayak","Wednesday":"rollerskate", "Thursday":"watercolor", "Friday":"drum"}
+weekdays=["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
 
-print(results)
+def lola_act (day):
+    for weekday,act in days.items():
+        if day==weekday:
+            print("On " + weekday + ", Lola likes to " + act)
 
-for items in nums:
-    data.append(items-10)
+def lola_sched (week):
+    for day in weekdays:
+        lola_act(day)
 
-print(data)
+lola_sched (weekdays)
 
-for name in hilt_class:
-    if name == 'Natalia':
-        print(name)
+print ("**********************")
+
+authors={"Charles Dicken":"1870", "William Thackeray":"1863","Anthony Trollope":"1882", "Gerard Manley Hopkins": "1889"}
+
+for author,year in authors.items():
+    print (author+" kicked the bucket in " + year)
